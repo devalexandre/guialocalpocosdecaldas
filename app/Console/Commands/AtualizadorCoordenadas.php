5401e58e89d11fa081067ad367f8e7c51e3b85f2
ricardo <ricardobboy_@hotmail.com>
2017-08-19 18:44:28 -0300

    Atualizada a classe que vai atualizar o cadastro de endereços com coordenadas

--- app/Console/Commands/AtualizadorCoordenadas.php
+++ app/Console/Commands/AtualizadorCoordenadas.php
@@ -1,29 +1,83 @@
 <?php
 namespace App\Console\Commands;
-use Illuminate\Support\Str;
+
+use App\Endereco;
 use Illuminate\Console\Command;
-use Symfony\Component\Console\Input\InputOption;
+
 class AtualizadorCoordenadas extends Command
 {
-    /**
-     * The console command name.
-     *
-     * @var string
-     */
+    protected $signature = 'coordenadas:atualizar {pagina}';
     protected $name = 'coordenadas:atualizar';
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = "Atualiza as coordendas dos clientes";
+    
     /**
-     * Execute the console command.
-     *
-     * @return void
+     * Atualiza a latitude e a longitude dos endereços das empresas 
      */
-    public function fire()
+    public function handle()
     {
-        $this->info("...");
+        $pagina = (int) $this->argument('pagina');
+
+        $keys = new \ArrayObject();
+        $keys->append('AIzaSyApjxPfyiREU8MCuWfypkzOiS8qYlG3Xqg');
+
+        $enderecos =  Endereco::
+            select('id', 'cidade', 'cep', 'numero', 'rua', 'uf')->
+            where('latitude', '=', '' )->
+            where('longitude', '=', '' )->
+            limit($pagina, 2000 * $pagina)->
+            get();
+
+        if(count($keys) < $pagina) {
+            $this->error('Não existe uma api para esta número de página!');
+            exit;
+        } 
+
+        $http = new \GuzzleHttp\Client();
+        $erro = 0;
+        $validarEndereco = 0;
+
+
+        $this->info( 'Processo iniciado' );
+        foreach ($enderecos as $endereco) {
+            $validarEndereco = 0;
+
+            echo $endereco->id . ' - ';
+
+            $coordenadas = json_decode(($http->request('GET', 'https://maps.googleapis.com/maps/api/geocode/json?key=' .
+                $keys->offsetGet($pagina - 1) , [
+                'query' => ['address' => "{$endereco->cidade},{$endereco->numero} {$endereco->rua} {$endereco->cep}"],
+                'timeout' => 2
+            ]))->getBody());        
+
+
+            if($coordenadas->status != 'OK') {
+
+                $this->error($coordenadas->status);
+
+                if($erro >= 5) {
+                    $this->erro = '';
+                    exit;
+                }
+
+                continue;
+            }
+
+            foreach ($coordenadas->results as $coordenada) {
+
+                $dadosCoordenadas = new \App\Helpers\GoogleMapaEndereco($coordenada, $endereco->uf, $endereco->cep);
+                
+                if($dadosCoordenadas->getLatitude() != '' && $dadosCoordenadas->getLongitude() != '') {
+                    
+                    $this->info('OK');
+                    continue;        
+                }
+            
+            }
+
+            $this->error('Endereco não encontrado.');
+
+        }
+
+        $this->info('Processo concluido');
     }
 }
